2667f0787ac4a25c3f61aab8c43c25c83abddb6a
Alex Malik <oleksii.malyk@gmail.com>
2020-10-18 13:10:10 +0300

    Add exceptions throwing, add documentation to methods and internal structures of Monobank class

--- src/Monobank/Monobank.cs
+++ src/Monobank/Monobank.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using System.Net.Mime;
 using System.Text;
-using Newtonsoft.Json;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Monobank
 {
@@ -15,17 +14,18 @@ namespace Monobank
     public class Monobank
     {
         private readonly HttpClient _httpClient;
-        private readonly DateTime? _lastTimeGetStatementCalled;
+        private readonly DateTime? _lastTimeGetStatementCalled = null;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Monobank"/> class with the personal token used to authenticate a person who makes a request.
+        /// Initializes a new instance of the <see cref="Monobank"/> class with the personal token used
+        /// to authenticate a person who makes a request.
         /// </summary>
         /// <param name="token">The token used to authenticate a person.</param>
-        /// <exception cref="ArgumentNullException">The <paramref name="token"/> is null or empty string.</exception>
+        /// <exception cref="InvalidTokenException">Thrown if <paramref name="token"/> is null or empty string.</exception>
         public Monobank(string token)
         {
             if (string.IsNullOrEmpty(token))
-                throw new ArgumentNullException(nameof(token));
+                throw new InvalidTokenException();
 
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri(API.Production);
@@ -33,10 +33,11 @@ namespace Monobank
         }
 
         /// <summary>
-        /// Gets a basic list of Monobank exchange rates. The server caches and updates information no more than once every 5 minutes.
+        /// Gets a basic list of Monobank exchange rates. The server caches and updates
+        /// information no more than once every 5 minutes.
         /// </summary>
+        /// <exception cref="MonobankRequestException">Thrown if request to Monobank's API failed.</exception>
         /// <returns>The list of Monobank exchange rates represented by <see cref="CurrencyInfo"/> objects.</returns>
-        /// <exception cref="MonobankException"></exception>
         public async Task<IEnumerable<CurrencyInfo>> GetCurrencyRatesAsync()
         {
             var (code, body) = await GetAsync(API.Bank.Currency);
@@ -50,8 +51,8 @@ namespace Monobank
         /// <summary>
         /// Gets a personal client's information and accounts. This function can be used only ones each 60 seconds.
         /// </summary>
+        /// <exception cref="MonobankRequestException">Thrown if request to Monobank's API failed.</exception>
         /// <returns>The <see cref="UserInfo"/> object containing client's personal information and accounts.</returns>
-        /// <exception cref="MonobankException"></exception>
         public async Task<UserInfo> GetUserInfoAsync()
         {
             var (code, body) = await GetAsync(API.Personal.ClientInfo);
@@ -70,13 +71,12 @@ namespace Monobank
         /// and 600 seconds. If no response is received on the third attempt, the function will be disabled.
         /// </summary>
         /// <param name="newHookUrl">The new webhook URL to set up.</param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="MonobankException"></exception>
+        /// <exception cref="InvalidWebhookUrlException">Thrown if provided <see cref="newHookUrl"/> argument is null or empty string.</exception>
+        /// <exception cref="MonobankRequestException">Thrown if request to Monobank's API failed.</exception>
         public async Task SetWebhookAsync(string newHookUrl)
         {
-            if (string.IsNullOrEmpty(newHookUrl))
-                throw new ArgumentNullException(newHookUrl);
+            if (string.IsNullOrEmpty(newHookUrl) /* TODO Consider validate URL. */)
+                throw new InvalidWebhookUrlException();
 
             var webhook = new Webhook(newHookUrl);
 
@@ -91,17 +91,19 @@ namespace Monobank
         /// <param name="account">The unique identifier of the account. Specify "0" to get default account.</param>
         /// <param name="from">The UTC date and time to get statement from.</param>
         /// <param name="to">The UTC date and time until the statement should be loaded.</param>
+        /// <exception cref="InvalidAccountException">Thrown if account number is null or empty string.</exception>
+        /// <exception cref="InvalidStatementPeriodException">Thrown if period between <see cref="from"/> and <see cref="to"/> is longer then 31 day and 1 hour.</exception>
+        /// <exception cref="ToFrequentCallException">Thrown if previous call of this method has been made less then minute ago.</exception>
+        /// <exception cref="MonobankRequestException">Thrown if request to Monobank's API failed.</exception>
         /// <returns>The list of <see cref="StatementItem"/>s.</returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="MonobankException"></exception>
         public async Task<IEnumerable<StatementItem>> GetStatementAsync(string account, DateTime from, DateTime to)
         {
             if (string.IsNullOrEmpty(account))
-                throw new ArgumentNullException(nameof(account));
+                throw new InvalidAccountException();
             if (IsValidInterval())
-                throw new MonobankException("The max period is 31 day + 1 hour.");
+                throw new InvalidStatementPeriodException();
             if (IsCallAvailable())
-                throw new MonobankException("Statement can be called ");
+                throw new ToFrequentCallException();
 
             var (code, body) = await GetAsync(API.Personal.Statement(account, @from, to));
             return code switch
@@ -116,15 +118,21 @@ namespace Monobank
                                       && (DateTime.UtcNow - _lastTimeGetStatementCalled.Value).TotalSeconds > Validation.StatementTimeoutBetweenCallsInSeconds;
         }
 
+        /// <summary>
+        /// Sends GET request to the Monobank's API with given <see cref="url"/> or url path.
+        /// </summary>
+        /// <param name="url">The URL or URL path to which make a request.</param>
+        /// <exception cref="MonobankRequestException">Thrown if request to Monobank's API failed.</exception>
+        /// <returns>Returns the tuple of HTTP Status Code and JSON Body received in response from Monobank's API.</returns>
         private async Task<(int Code, string Body)> GetAsync(string url)
         {
             try
             {
                 var response = await _httpClient.GetAsync(url);
                 return (
-                    Code: (int)response.StatusCode, 
+                    Code: (int) response.StatusCode,
                     Body: await response.Content.ReadAsStringAsync());
-            
+
                 // TODO Consider throw an exception on any not 200 status.
             }
             catch (HttpRequestException exception)
@@ -133,6 +141,12 @@ namespace Monobank
             }
         }
 
+        /// <summary>
+        /// Sends POST request to the Monobank's API with given <see cref="url"/> or url path.
+        /// </summary>
+        /// <param name="url">The URL or URL path to which make a request.</param>
+        /// <exception cref="MonobankRequestException">Thrown if request to Monobank's API failed.</exception>
+        /// <returns>Returns the tuple of HTTP Status Code and JSON Body received in response from Monobank's API.</returns>
         private async Task<(int Code, string Body)> PostAsync<T>(string url, T value)
         {
             try
@@ -141,9 +155,9 @@ namespace Monobank
                 var content = new StringContent(jsonString, Encoding.UTF8, MediaTypeNames.Application.Json);
                 var response = await _httpClient.PostAsync(url, content);
                 return (
-                    Code: (int)response.StatusCode, 
+                    Code: (int) response.StatusCode,
                     Body: await response.Content.ReadAsStringAsync());
-            
+
                 // TODO Consider throw an exception on any not 200 status.
             }
             catch (HttpRequestException exception)
@@ -152,10 +166,13 @@ namespace Monobank
             }
         }
 
+        /// <summary>
+        /// Encapsulates constants related to API requests, URLs and URL parts.
+        /// </summary>
         private static class API
         {
             public const string Production = "https://api.monobank.ua";
-            
+
             public static class Bank
             {
                 public const string Currency = "/bank/currency";
@@ -176,11 +193,17 @@ namespace Monobank
             }
         }
 
+        /// <summary>
+        /// Encapsulates constants related to HTTP Headers used when making calls to Monobank's API.
+        /// </summary>
         private static class RequestHeaders
         {
             public const string XToken = "X-Token";
         }
 
+        /// <summary>
+        /// Encapsulates constants used by validation methods and checks.
+        /// </summary>
         private static class Validation
         {
             public const int MaxStatementTimeSpanInSeconds = 2682000;
